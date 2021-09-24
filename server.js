75e182edf46ff2dd10740113a40d6e3183d4d072
SunOn9 <lehophattai@gmail.com>
2021-09-25 06:43:14 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const config = require("./app/config");
+const setupContactRoutes = require("./app/routes/contact.routes");
 
 
 const app = express();
@@ -14,6 +15,8 @@ app.use(express.urlencoded({ extended: true}));
 app.get("/",(req, res) =>{
     res.json({ message: "Welcome to contact book application."});
 });
+
+setupContactRoutes(app);
 
 const PORT = config.app.port;
 app.listen(PORT, () => {
